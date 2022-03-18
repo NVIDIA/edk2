4f2c0eddad60bca546419175567a51f56a835e80
Swatisri Kantamsetti <swatisrik@nvidia.com>
2022-03-18 11:20:19 -0600

    DynamicTablesPkg: MPAM Generator and supporting files
    ACPI header, MSC and Resource Nodes are populated
    in the MPAM Table
    
    Signed-off-by: Swatisri Kantamsetti <swatisrik@nvidia.com>
    Reviewed-by: Jeff Brasen <jbrasen@nvidia.com>

--- DynamicTablesPkg/Include/ArmNameSpaceObjects.h
+++ DynamicTablesPkg/Include/ArmNameSpaceObjects.h
@@ -73,6 +73,9 @@ typedef enum ArmObjectID {
   EArmObjPccSubspaceType5Info,                                 ///< 48 - Pcc Subspace Type 5 Info
   EArmObjEtInfo,                                               ///< 49 - Embedded Trace Extension/Module Info
   EArmObjPsdInfo,                                              ///< 50 - P-State Dependency (PSD) Info
+  EArmObjMscNodeInfo,                                          ///< 51 - Msc Memory System Controller Node Info
+  EArmObjResNodeInfo,                                          ///< 52 - Res Resource Node Info
+  EArmObjFuncDepInfo,                                          ///< 53 - Func Dep Info
   EArmObjMax
 } EARM_OBJECT_ID;
 
@@ -1344,6 +1347,88 @@ typedef struct CmArmEtInfo {
     ID: EArmObjPsdInfo
 */
 typedef AML_PSD_INFO CM_ARM_PSD_INFO;
+
+/** A structure that describes Memory System Controller Node.
+
+    MPAM Memory System Component Nodes are described by
+    this object.
+
+  ID: EArmObjMscNodeInfo
+*/
+typedef struct CmArmMscNodeInfo {
+  /// An unique token used to identify this object
+  CM_OBJECT_TOKEN    Token;
+
+  /// Identifier
+  UINT32             Identifier;
+  /// MPAM Base Address
+  UINT64             BaseAddress;
+  /// MMIO Size
+  UINT32             MmioSize;
+  /// Overflow Interrupt
+  UINT32             OverflowInterrupt;
+  /// Overflow Interrupt Flags
+  UINT32             OverflowInterruptFlags;
+  /// Overflow Interrupt Affinity
+  UINT32             OverflowInterruptAff;
+  /// Error Interrupt
+  UINT32             ErrorInterrupt;
+  /// Error Interrupt Flags
+  UINT32             ErrorInterruptFlags;
+  /// Error Interrupt Affinity
+  UINT32             ErrorInterruptAff;
+  /// Not Ready Signal time
+  UINT32             MaxNRdyUsec;
+  /// Linked Device HWID
+  UINT64             LinkedDeviceHwId;
+  /// Linked Device Instance ID
+  UINT32             LinkedDeviceInstanceHwId;
+  /// Number of Resource nodes
+  UINT32             NumResourceNodes;
+  /// Reference token for the list of resource nodes
+  // CM_OBJECT_TOKEN    ResourceNodeListToken;
+} CM_ARM_MSC_NODE_INFO;
+
+/** A structure that describes Resource Node info.
+
+    MPAM Resource Nodes are described by
+    this object.
+
+  ID: EArmObjResNodeInfo
+*/
+typedef struct CmArmResNodeInfo {
+  /// An unique token used to identify this object
+  CM_OBJECT_TOKEN    Token;
+
+  /// Identifier
+  UINT32             Identifier;
+  /// RIS Index
+  UINT8              RisIndex;
+  /// Locator Type
+  UINT8              LocatorType;
+  /// Locator
+  UINT64             Locator1;
+  UINT32             Locator2;
+  /// Num functional dependencies
+  UINT32             NumFuncDep;
+  /// Reference token for the list of resource nodes
+  CM_OBJECT_TOKEN    FuncDepListToken;
+} CM_ARM_RESOURCE_NODE_INFO;
+
+/** A structure that describes Functional Dependencies
+    between MPAM MSC nodes.
+
+    MPAM Functional Dependency descriptors are described by
+    this object.
+
+  ID: EArmObjFuncDepInfo
+*/
+typedef struct CmArmFuncDepInfo {
+  /// An unique token used to identify this object
+  CM_OBJECT_TOKEN    Token;
+  /// Producer
+  UINT32             Producer;
+} CM_ARM_FUNC_DEP_INFO;
 
 #pragma pack()
 
