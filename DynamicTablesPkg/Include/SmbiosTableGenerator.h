d5d7803b7a4c79880686b198effb2d460854a4b2
Girish Mahadevan <gmahadevan@nvidia.com>
2022-07-06 15:18:00 -0600

    DynamicTablesPkg: Add SMBIOS table generation
    Add the SMBIOS Table generator code to the DynamicTablesPkg.
    
    This change includes adding new logic to the DynamicTableManager
    to process and add SMBIOS tables and augmenting the existing SMBIOS
    Factory generator to include installing multiple SMBIOS tables.
    Also included is running the SMBIOS and ACPI table generation as part
    of the SMBIOS and ACPI protocol GUID callback notifications respectively.
    
    Signed-off-by: Girish Mahadevan <gmahadevan@nvidia.com>
    Reviewed-by: Jeff Brasen <jbrasen@nvidia.com>

--- DynamicTablesPkg/Include/SmbiosTableGenerator.h
+++ DynamicTablesPkg/Include/SmbiosTableGenerator.h
@@ -10,7 +10,7 @@
 #define SMBIOS_TABLE_GENERATOR_H_
 
 #include <IndustryStandard/SmBios.h>
-
+#include <Protocol/Smbios.h>
 #include <TableGenerator.h>
 
 #pragma pack(1)
@@ -121,12 +121,21 @@ typedef enum StdSmbiosTableGeneratorId {
             ETableGeneratorNameSpaceStd,        \
             TableId                             \
             )
+#define MAX_SMBIOS_HANDLES               (255)
 
 /** Forward declarations.
 */
 typedef struct ConfigurationManagerProtocol EDKII_CONFIGURATION_MANAGER_PROTOCOL;
 typedef struct CmStdObjSmbiosTableInfo      CM_STD_OBJ_SMBIOS_TABLE_INFO;
 typedef struct SmbiosTableGenerator         SMBIOS_TABLE_GENERATOR;
+typedef struct DynamicTableFactoryProtocol  EDKII_DYNAMIC_TABLE_FACTORY_PROTOCOL;
+typedef UINTN CM_OBJECT_TOKEN;
+
+typedef struct SmbiosHandleCmObjMap {
+  ESTD_SMBIOS_TABLE_ID  SmbiosTableId;
+  SMBIOS_HANDLE         SmbiosTblHandle;
+  CM_OBJECT_TOKEN       SmbiosCmToken;
+} SMBIOS_HANDLE_MAP;
 
 /** This function pointer describes the interface to SMBIOS table build
     functions provided by the SMBIOS table generator and called by the
@@ -143,9 +152,11 @@ typedef struct SmbiosTableGenerator         SMBIOS_TABLE_GENERATOR;
 **/
 typedef EFI_STATUS (*SMBIOS_TABLE_GENERATOR_BUILD_TABLE) (
   IN  CONST SMBIOS_TABLE_GENERATOR                        *Generator,
+  IN  CONST EDKII_DYNAMIC_TABLE_FACTORY_PROTOCOL  *CONST  TableFactoryProtocol,
   IN        CM_STD_OBJ_SMBIOS_TABLE_INFO          *CONST  SmbiosTableInfo,
   IN  CONST EDKII_CONFIGURATION_MANAGER_PROTOCOL  *CONST  CfgMgrProtocol,
-  OUT       SMBIOS_STRUCTURE                              **Table
+  OUT       SMBIOS_STRUCTURE                              **Table,
+  OUT       CM_OBJECT_TOKEN                               *CmObjToken
   );
 
 /** This function pointer describes the interface to used by the
@@ -163,9 +174,103 @@ typedef EFI_STATUS (*SMBIOS_TABLE_GENERATOR_BUILD_TABLE) (
 **/
 typedef EFI_STATUS (*SMBIOS_TABLE_GENERATOR_FREE_TABLE) (
   IN  CONST SMBIOS_TABLE_GENERATOR                        *Generator,
+  IN  CONST EDKII_DYNAMIC_TABLE_FACTORY_PROTOCOL  *CONST  TableFactoryProtocol,
   IN  CONST CM_STD_OBJ_SMBIOS_TABLE_INFO          *CONST  SmbiosTableInfo,
   IN  CONST EDKII_CONFIGURATION_MANAGER_PROTOCOL  *CONST  CfgMgrProtocol,
   IN        SMBIOS_STRUCTURE                              **Table
+  );
+
+/** This function pointer describes the interface to SMBIOS table build
+    functions provided by the SMBIOS table generator and called by the
+    Table Manager to build an SMBIOS table.
+
+  @param [in]  Generator       Pointer to the SMBIOS table generator.
+  @param [in]  SmbiosTableInfo Pointer to the SMBIOS table information.
+  @param [in]  CfgMgrProtocol  Pointer to the Configuration Manager
+                               Protocol interface.
+  @param [out] Table           Pointer to the generated SMBIOS table.
+
+  @return EFI_SUCCESS  If the table is generated successfully or other
+                        failure codes as returned by the generator.
+**/
+typedef EFI_STATUS (*SMBIOS_TABLE_GENERATOR_BUILD_TABLEEX) (
+  IN  CONST SMBIOS_TABLE_GENERATOR                        *Generator,
+  IN  CONST EDKII_DYNAMIC_TABLE_FACTORY_PROTOCOL   *CONST  TableFactoryProtocol,
+  IN        CM_STD_OBJ_SMBIOS_TABLE_INFO          *CONST  SmbiosTableInfo,
+  IN  CONST EDKII_CONFIGURATION_MANAGER_PROTOCOL  *CONST  CfgMgrProtocol,
+  OUT       SMBIOS_STRUCTURE                              ***Table,
+  OUT       CM_OBJECT_TOKEN                               **CmObjectToken,
+  OUT       UINTN                                 *CONST  TableCount
+  );
+
+/** This function pointer describes the interface to used by the
+    Table Manager to give the generator an opportunity to free
+    any resources allocated for building the SMBIOS table.
+
+  @param [in]  Generator       Pointer to the SMBIOS table generator.
+  @param [in]  SmbiosTableInfo Pointer to the SMBIOS table information.
+  @param [in]  CfgMgrProtocol  Pointer to the Configuration Manager
+                               Protocol interface.
+  @param [in]  Table           Pointer to the generated SMBIOS table.
+
+  @return  EFI_SUCCESS If freed successfully or other failure codes
+                        as returned by the generator.
+**/
+typedef EFI_STATUS (*SMBIOS_TABLE_GENERATOR_FREE_TABLEEX) (
+  IN  CONST SMBIOS_TABLE_GENERATOR                        *Generator,
+  IN  CONST EDKII_DYNAMIC_TABLE_FACTORY_PROTOCOL  *CONST  TableFactoryProtocol,
+  IN  CONST CM_STD_OBJ_SMBIOS_TABLE_INFO          *CONST  SmbiosTableInfo,
+  IN  CONST EDKII_CONFIGURATION_MANAGER_PROTOCOL  *CONST  CfgMgrProtocol,
+  IN        SMBIOS_STRUCTURE                              ***Table,
+  IN        CM_OBJECT_TOKEN                               **CmObjectToken,
+  IN  CONST UINTN                                          TableCount
+  );
+
+/** This function pointer describes the interface to used by the
+    Table Manager to give the generator an opportunity to add
+    an SMBIOS Handle.
+
+  This function is called by the Dynamic Table Manager to add a newly added
+  SMBIOS Table OR it can be called by an SMBIOS Table generator to create
+  and add a new SMBIOS Handle if there is a reference to another table and
+  if there is a generator for that table that hasn't been called yet.
+
+  @param [in]  Smbios         Pointer to the SMBIOS protocol.
+  @param [in]  SmbiosHandle   Pointer to an SMBIOS handle (either generated by
+                              SmbiosDxe Driver or  SMBIOS_HANDLE_PI_RESERVED
+                              if a handle needs to be generated).
+  @param [in]  CmObjectToken  The CM Object token for that is used to generate
+                              SMBIOS record.
+  @param [in]  SmbiosId       The SMBIOS table generator Id.
+
+  @retval EFI_SUCCESS           Success.
+  @retval EFI_OUT_OF_RESOURCES  Unable to add the handle.
+  @retval EFI_NOT_FOUND         The requested generator is not found
+                                in the list of registered generators.
+**/
+typedef EFI_STATUS (*EDKII_DYNAMIC_TABLE_FACTORY_SMBIOS_TABLE_ADD_HANDLE) (
+  IN  EFI_SMBIOS_PROTOCOL  *Smbios,
+  IN  SMBIOS_HANDLE        *SmbiosHandle,
+  IN  CM_OBJECT_TOKEN      CmObjectToken,
+  IN ESTD_SMBIOS_TABLE_ID  SmbiosId
+  );
+
+/** This function pointer describes the interface to used by the
+    Table Manager to give the generator an opportunity to find
+    an SMBIOS Handle.
+
+  This function is called by the SMBIOS table generator to find an SMBIOS
+  handle needed as part of generating an SMBIOS Table.
+
+  @param [in]  CmObjectToken    The CM Object token used to generate the SMBIOS
+                                record.
+
+  @retval EFI_SUCCESS           Success.
+  @retval EFI_NOT_FOUND         The requested generator is not found
+                                in the list of registered generators.
+**/
+typedef SMBIOS_HANDLE_MAP * (*EDKII_DYNAMIC_TABLE_FACTORY_SMBIOS_TABLE_GET_HANDLE) (
+  IN  CM_OBJECT_TOKEN      CmObjectToken
   );
 
 /** The SMBIOS_TABLE_GENERATOR structure provides an interface that the
@@ -189,6 +294,14 @@ typedef struct SmbiosTableGenerator {
       allocated for building the SMBIOS table.
   */
   SMBIOS_TABLE_GENERATOR_FREE_TABLE     FreeTableResources;
+
+  /// SMBIOS table extended build function pointer.
+  SMBIOS_TABLE_GENERATOR_BUILD_TABLEEX  BuildSmbiosTableEx;
+
+  /** The function to free any resources
+      allocated for building the SMBIOS table.
+  */
+  SMBIOS_TABLE_GENERATOR_FREE_TABLEEX   FreeTableResourcesEx;
 } SMBIOS_TABLE_GENERATOR;
 
 /** Register SMBIOS table factory generator.
@@ -227,6 +340,54 @@ EFI_STATUS
 EFIAPI
 DeregisterSmbiosTableGenerator (
   IN CONST SMBIOS_TABLE_GENERATOR                 *CONST  Generator
+  );
+
+/** Add SMBIOS Handle.
+
+  This function is called by the Dynamic Table Manager to add a newly added
+  SMBIOS Table OR it can be called by an SMBIOS Table generator to create
+  and add a new SMBIOS Handle if there is a reference to another table and
+  if there is a generator for that table that hasn't been called yet.
+
+  @param [in]  Smbios         Pointer to the SMBIOS protocol.
+  @param [in]  SmbiosHandle   Pointer to an SMBIOS handle (either generated by
+                              SmbiosDxe Driver or  SMBIOS_HANDLE_PI_RESERVED
+                              if a handle needs to be generated).
+  @param [in]  CmObjectToken  The CM Object token for that is used to generate
+                              SMBIOS record.
+  @param [in]  SmbiosId       The SMBIOS table generator Id.
+
+  @retval EFI_SUCCESS           Success.
+  @retval EFI_OUT_OF_RESOURCES  Unable to add the handle.
+  @retval EFI_NOT_FOUND         The requested generator is not found
+                                in the list of registered generators.
+**/
+EFI_STATUS
+EFIAPI
+AddSmbiosHandle (
+  IN EFI_SMBIOS_PROTOCOL   *Smbios,
+  IN SMBIOS_HANDLE         *SmbiosHandle,
+  IN CM_OBJECT_TOKEN       CmObjectToken,
+  IN ESTD_SMBIOS_TABLE_ID  SmbiosId
+  );
+
+/** Find SMBIOS Handle given the CM Object token used to generate the SMBIOS
+    record..
+
+  This function is called by the SMBIOS table generator to find an SMBIOS
+  handle needed as part of generating an SMBIOS Table.
+
+  @param [in]  CmObjectToken    The CM Object token used to generate the SMBIOS
+                                record.
+
+  @retval EFI_SUCCESS           Success.
+  @retval EFI_NOT_FOUND         The requested generator is not found
+                                in the list of registered generators.
+**/
+SMBIOS_HANDLE_MAP *
+EFIAPI
+FindSmbiosHandle (
+  IN CM_OBJECT_TOKEN CmObjectToken
   );
 
 #pragma pack()
