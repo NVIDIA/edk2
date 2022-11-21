2f334c4e7dab5359dc20f43e531ac61a01d782b8
Nickle Wang <nicklew@nvidia.com>
2022-11-21 14:36:36 +0800

    RedfishClientPkg: add Redfish driver and library from edk2-staging
    Add RedfishPlatformConfigDxe and HiiUtilityLib from edk2-staging
    
    Signed-off-by: Nickle Wang <nicklew@nvidia.com>
    Reviewed-by: Ashish Singhal <ashishsingha@nvidia.com>
    Reviewed-by: Nick Ramirez <nramirez@nvidia.com>

---
+++ RedfishClientPkg/ConverterLib/edk2library/ConverterCommonLib/RedfishCsCommon.h
@@ -0,0 +1,14 @@
+/** @file
+  Wrapper file for RedfishCsCommon.h
+
+  (C) Copyright 2021 Hewlett Packard Enterprise Development LP<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef WRAPPER_REDFISH_CS_COMMON_H_
+#define WRAPPER_REDFISH_CS_COMMON_H_
+
+#include "../../include/RedfishCsCommon.h"
+
+#endif
