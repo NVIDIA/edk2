1136f77eabc15c9ae0816693d124540416f9c4c2
Abner Chang <abner.chang@amd.com>
2022-12-19 21:18:00 +0800

    RedfishPkg/RedfishHostInterface: Platform Redfish HI notification
    For some use cases, Redfish host interface table relies on
    the certain EFI protocols installation at the driver connection.
    Redfish host interface DXE driver is not able to build the
    SMBIOS type 42h record at driver entry point. This patch adds
    the mechanism in Redfish host interface DXE driver to listen
    to EFI protocol installed by platform library that indicates
    the necessary information is ready for building SMBIOS 42h
    record.
    
    Signed-off-by: Abner Chang <abner.chang@amd.com>
    Cc: Nickle Wang <nicklew@nvidia.com>
    Cc: Igor Kulchytskyy <igork@ami.com>
    Reviewed-by: Nickle Wang <nicklew@nvidia.com>
    Reviewed-by: Nick Ramirez <nramirez@nvidia.com>
    Tested-by: Nickle Wang <nicklew@nvidia.com>
    Tested-by: Ashish Singhal <ashishsingha@nvidia.com>
    Reviewed-by: Ashish Singhal <ashishsingha@nvidia.com>

--- RedfishPkg/Include/Library/RedfishHostInterfaceLib.h
+++ RedfishPkg/Include/Library/RedfishHostInterfaceLib.h
@@ -20,7 +20,7 @@
 /**
   Get platform Redfish host interface device descriptor.
 
-  @param[in] DeviceType         Pointer to retrieve device type.
+  @param[out] DeviceType        Pointer to retrieve device type.
   @param[out] DeviceDescriptor  Pointer to retrieve REDFISH_INTERFACE_DATA, caller has to free
                                 this memory using FreePool().
   @retval EFI_SUCCESS     Device descriptor is returned successfully in DeviceDescriptor.
@@ -29,7 +29,7 @@
 **/
 EFI_STATUS
 RedfishPlatformHostInterfaceDeviceDescriptor (
-  IN UINT8                    *DeviceType,
+  OUT UINT8                   *DeviceType,
   OUT REDFISH_INTERFACE_DATA  **DeviceDescriptor
   );
 
@@ -41,7 +41,7 @@ RedfishPlatformHostInterfaceDeviceDescriptor (
   @param[in, out] ProtocolRecord  Pointer to retrieve the first or the next protocol record.
                                   caller has to free the new protocol record returned from
                                   this function using FreePool().
-  param[in] IndexOfProtocolData   The index of protocol data.
+  @param[in] IndexOfProtocolData  The index of protocol data.
 
   @retval EFI_SUCCESS     Protocol records are all returned.
   @retval EFI_NOT_FOUND   No more protocol records.
@@ -64,6 +64,27 @@ RedfishPlatformHostInterfaceProtocolData (
 EFI_STATUS
 RedfishPlatformHostInterfaceUSBSerialNumber (
   OUT CHAR8  **SerialNumber
+  );
+
+/**
+  Get the EFI protocol GUID installed by platform library which
+  indicates the necessary information is ready for building
+  SMBIOS 42h record.
+
+  @param[out] InformationReadinessGuid  Pointer to retrive the protocol
+                                        GUID.
+
+  @retval EFI_SUCCESS          Notification is required for building up
+                               SMBIOS type 42h record.
+  @retval EFI_UNSUPPORTED      Notification is not required for building up
+                               SMBIOS type 42h record.
+  @retval EFI_ALREADY_STARTED  Platform host information is already ready.
+  @retval Others               Other errors.
+**/
+
+EFI_STATUS
+RedfishPlatformHostInterfaceNotification (
+  OUT EFI_GUID  **InformationReadinessGuid
   );
 
 #endif
