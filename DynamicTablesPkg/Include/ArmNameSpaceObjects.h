53d840c0890e483fc11123e9fc91689dd5b62b15
Dat Mach <dmach@nvidia.com>
2023-01-09 17:54:58 -0800

    DynamicTablesPkg: ACPI TPM2 generator
    Generate ACPI TPM2 table using the information obtained from
    Tpm2InterfaceInfo CM object.
    
    Signed-off-by: Dat Mach <dmach@nvidia.com>
    Reviewed-by: Nick Ramirez <nramirez@nvidia.com>
    Reviewed-by: Swatisri Kantamsetti <swatisrik@nvidia.com>
    Reviewed-by: Ashish Singhal <ashishsingha@nvidia.com>
    Tested-by: Ashish Singhal <ashishsingha@nvidia.com>

--- DynamicTablesPkg/Include/ArmNameSpaceObjects.h
+++ DynamicTablesPkg/Include/ArmNameSpaceObjects.h
@@ -71,6 +71,7 @@ typedef enum ArmObjectID {
   EArmObjSystemLocalityInfo,           ///< 46 - Relative Distance Info
   EArmObjMemoryDeviceInfo,             ///< 47 - Memory Device Information
   EArmObjLocalityLatencyBandwidthInfo, ///< 48 - System Locality Latency and Bandwidth Info
+  EArmObjTpm2InterfaceInfo,            ///< 49 - TPM Interface Info
   EArmObjMax
 } EARM_OBJECT_ID;
 
@@ -1270,6 +1271,38 @@ typedef struct CmArmObjLocalityLatencyBandwidthInfo {
   UINT32                                                                          *TargetProximityDomainList;
   UINT16                                                                          *LatencyBandwidthEntries;
 } CM_ARM_LOCALITY_LATENCY_BANDWIDTH_INFO;
+
+/** A structure that describes TPM interface and access method.
+
+  TCG ACPI Specification 2.0
+
+  ID: EArmObjTpm2InterfaceInfo,
+*/
+typedef struct CmArmTpm2InterfaceInfo {
+  /** Platform Class
+        0: Client platform
+        1: Server platform
+  */
+  UINT16    PlatformClass;
+
+  /** Physical address of the Control Area */
+  UINT64    AddressOfControlArea;
+
+  /** The Start Method selector determines which mechanism the
+      device driver uses to notify the TPM 2.0 device that a
+      command is available for processing.
+  */
+  UINT32    StartMethod;
+
+  /** Start method specific parameters */
+  UINT8     StartMethodParameters[12];
+
+  /** Log Area Minimum Length */
+  UINT32    Laml;
+
+  /** Log Area Start Address */
+  UINT64    Lasa;
+} CM_ARM_TPM2_INTERFACE_INFO;
 
 #pragma pack()
 
