5b1f402d84f547356cdcd5a3196ebd7d4b3c88d0
Simon Wang <simowang@nvidia.com>
2023-03-13 06:41:25 +0000

    RedfishClientPkg: Reduce the header file dependency
    Move REDFISH_FEATURE_ARRAY_TYPE_CONFIG_LANG and
    REDFISH_FEATURE_ARRAY_TYPE_CONFIG_LANG_LIST from
    RedfishFeatureUtilityLib.h to
    EdkIIRedfishInterchangeData.h. Also, remove
    unnecessary jansson.h in RedfishDataTypeDef.h.
    
    Signed-off-by: Simon Wang <simowang@nvidia.com>
    Reviewed-by: Nickle Wang <nicklew@nvidia.com>
    Reviewed-by: Nick Ramirez <nramirez@nvidia.com>

--- RedfishClientPkg/Include/Protocol/EdkIIRedfishInterchangeData.h
+++ RedfishClientPkg/Include/Protocol/EdkIIRedfishInterchangeData.h
@@ -2,6 +2,7 @@
   This file defines the EDKII_REDFISH_FEATURE_INTERCHANGE_DATA_PROTOCOL interface.
 
   (C) Copyright 2022 Hewlett Packard Enterprise Development LP<BR>
+  Copyright (c) 2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
@@ -10,7 +11,21 @@
 #ifndef EDKII_REDFISH_FEATURE_INTERCHANGE_DATA_H_
 #define EDKII_REDFISH_FEATURE_INTERCHANGE_DATA_H_
 
-#include <Library/RedfishFeatureUtilityLib.h>
+//
+// Definition of REDFISH_FEATURE_ARRAY_TYPE_CONFIG_LANG
+//
+typedef struct {
+  UINTN         Index;
+  EFI_STRING    ConfigureLang;
+} REDFISH_FEATURE_ARRAY_TYPE_CONFIG_LANG;
+
+//
+// Definition of REDFISH_FEATURE_ARRAY_TYPE_CONFIG_LANG_LIST
+//
+typedef struct {
+  UINTN                                     Count;
+  REDFISH_FEATURE_ARRAY_TYPE_CONFIG_LANG    *List;
+} REDFISH_FEATURE_ARRAY_TYPE_CONFIG_LANG_LIST;
 
 typedef struct _EDKII_REDFISH_FEATURE_INTERCHANGE_DATA_PROTOCOL EDKII_REDFISH_FEATURE_INTERCHANGE_DATA_PROTOCOL;
 
@@ -27,15 +42,15 @@ typedef enum {
 } RESOURCE_INFORMATION_EXCHANGE_TYPE;
 
 typedef struct {
-  RESOURCE_INFORMATION_EXCHANGE_TYPE Type;
-  EFI_STRING     ParentUri;          ///< The parent URI (in configure language) of the resource to process.
-  EFI_STRING     PropertyName;       ///< The property name of the resource to process.
-  EFI_STRING     FullUri;            ///< The full URI (in configure language) of the resource to process.
+  RESOURCE_INFORMATION_EXCHANGE_TYPE    Type;
+  EFI_STRING                            ParentUri;    ///< The parent URI (in configure language) of the resource to process.
+  EFI_STRING                            PropertyName; ///< The property name of the resource to process.
+  EFI_STRING                            FullUri;      ///< The full URI (in configure language) of the resource to process.
 } RESOURCE_INFORMATION_SEND;
 
 typedef struct {
-  RESOURCE_INFORMATION_EXCHANGE_TYPE Type;
-  REDFISH_FEATURE_ARRAY_TYPE_CONFIG_LANG_LIST ConfigureLanguageList;
+  RESOURCE_INFORMATION_EXCHANGE_TYPE             Type;
+  REDFISH_FEATURE_ARRAY_TYPE_CONFIG_LANG_LIST    ConfigureLanguageList;
 } RESOURCE_INFORMATION_RETURNED;
 
 typedef struct {
@@ -44,9 +59,9 @@ typedef struct {
 } RESOURCE_INFORMATION_EXCHANGE;
 
 struct _EDKII_REDFISH_FEATURE_INTERCHANGE_DATA_PROTOCOL {
-  RESOURCE_INFORMATION_EXCHANGE *ResourceInformationExchage;
+  RESOURCE_INFORMATION_EXCHANGE    *ResourceInformationExchage;
 };
 
-extern EFI_GUID gEdkIIRedfishFeatureInterchangeDataProtocolGuid;
+extern EFI_GUID  gEdkIIRedfishFeatureInterchangeDataProtocolGuid;
 
 #endif
