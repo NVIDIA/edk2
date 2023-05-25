8210c5116890d5bb7d52312a71e048d611ad8a83
Nickle Wang <nicklew@nvidia.com>
2023-05-25 09:09:22 +0800

    RedfishClientPkg: introduce Redfish HTTP cache library
    Introduce RedfishHttpCacheLib to improve HTTP GET performance
    in Redfish feature drivers. Feature drivers often query same
    Redfish resource multiple times for different purpose. Add
    HTTP cache mechanism to improve the performance.
    
    Signed-off-by: Nickle Wang <nicklew@nvidia.com>
    Reviewed-by: Simon Wang (SW-GPU) <simowang@nvidia.com>
    Reviewed-by: Nick Ramirez <nramirez@nvidia.com>

---
+++ RedfishClientPkg/Library/RedfishHttpCacheLib/RedfishHttpCacheLibInternal.h
@@ -0,0 +1,58 @@
+/** @file
+  This file defines the Redfish HTTP cache library internal headers.
+
+  Copyright (c) 2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef REDFISH_HTTP_CACHE_INTERNAL_LIB_H_
+#define REDFISH_HTTP_CACHE_INTERNAL_LIB_H_
+
+#include <Uefi.h>
+#include <RedfishBase.h>
+
+#include <Library/UefiLib.h>
+#include <Library/BaseLib.h>
+#include <Library/DebugLib.h>
+#include <Library/RedfishLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/MemoryAllocationLib.h>
+#include <Library/RedfishHttpCacheLib.h>
+
+#define REDFISH_HTTP_CACHE_LIST_SIZE   0x05
+#define REDFISH_HTTP_GET_RETRY_MAX     0x02
+#define REDFISH_HTTP_RETRY_WAIT        (1 * 1000000U)  ///< 1 second
+#define REDFISH_HTTP_CACHE_DEBUG       DEBUG_VERBOSE
+#define REDFISH_HTTP_CACHE_DEBUG_DUMP  DEBUG_VERBOSE
+
+///
+/// Definition of REDFISH_HTTP_CACHE_DATA
+///
+typedef struct {
+  LIST_ENTRY          List;
+  EFI_STRING          Uri;
+  UINTN               HitCount;
+  REDFISH_RESPONSE    *Response;
+} REDFISH_HTTP_CACHE_DATA;
+
+#define REDFISH_HTTP_CACHE_FROM_LIST(a)  BASE_CR (a, REDFISH_HTTP_CACHE_DATA, List)
+
+///
+/// Definition of REDFISH_HTTP_CACHE_LIST
+///
+typedef struct {
+  LIST_ENTRY    Head;
+  UINTN         Count;
+  UINTN         Capacity;
+} REDFISH_HTTP_CACHE_LIST;
+
+///
+/// Definition of REDFISH_HTTP_CACHE_PRIVATE
+///
+typedef struct {
+  REDFISH_HTTP_CACHE_LIST    CacheList;
+} REDFISH_HTTP_CACHE_PRIVATE;
+
+#endif
