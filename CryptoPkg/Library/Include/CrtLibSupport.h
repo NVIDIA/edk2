feb8f94b46ce80fe2aab662ea4a3b207615969f0
Jeff Brasen <jbrasen@nvidia.com>
2024-01-12 08:14:21 -0800

    CryptoPkg/MbedTls: Update to v3.6.2
    Update to resolve to following CVEs
    CVE-2023-45199
    CVE-2023-43615
    CVE-2024-28960
    CVE-2023-52353
    CVE-2024-23775
    CVE-2024-23170
    CVE-2024-23744
    CVE-2024-49195
    CVE-2024-45158
    CVE-2024-45159
    CVE-2024-45157
    
    BaseCryptLibMbedTls needs to be updated to compile against the new
    version.  This library is not in use by NVIDIA.  Rather than update code
    we do not use, we'll simply comment the functions that fail to compile
    so that we can satisfy edk2's CI.
    
    Signed-off-by: Jeff Brasen <jbrasen@nvidia.com>

--- CryptoPkg/Library/Include/CrtLibSupport.h
+++ CryptoPkg/Library/Include/CrtLibSupport.h
@@ -430,6 +430,12 @@ strpbrk (
   const char  *accept
   );
 
+void
+explicit_bzero (
+  void *,
+  size_t
+  );
+
 //
 // Macros that directly map functions to BaseLib, BaseMemoryLib, and DebugLib functions
 //
