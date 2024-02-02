49e77dc86902da16fe6a86fb83dc1f6fabb59d85
Dat Mach <dmach@nvidia.com>
2024-02-01 17:40:52 -0800

    DynamicTablesPkg: Add parser for Tpm2 CM object
    Update the CM Object parser to add support for parsing the
    CM_ARM_TPM2_INTERFACE_INFO object.
    
    Signed-off-by: Dat Mach <dmach@nvidia.com>
    Reviewed-by: Jeshua Smith <jeshuas@nvidia.com>

--- DynamicTablesPkg/Library/Common/TableHelperLib/ConfigurationManagerObjectParser.c
+++ DynamicTablesPkg/Library/Common/TableHelperLib/ConfigurationManagerObjectParser.c
@@ -675,6 +675,17 @@ STATIC CONST CM_OBJ_PARSER  CmArmPsdInfoParser[] = {
   { "NumProc",   4, "0x%x", NULL },
 };
 
+/** A parser for EArmObjTpm2InterfaceInfo.
+*/
+STATIC CONST CM_OBJ_PARSER  CmArmTpm2InterfaceInfo[] = {
+  { "PlatformClass",         sizeof (UINT16), "0x%x",   NULL    },
+  { "AddressOfControlArea",  sizeof (UINT64), "0x%llx", NULL    },
+  { "StartMethod",           sizeof (UINT32), "0x%x",   NULL    },
+  { "StartMethodParameters", 12,              NULL,     HexDump },
+  { "Laml",                  sizeof (UINT32), "0x%x",   NULL    },
+  { "Lasa",                  sizeof (UINT64), "0x%llx", NULL    },
+};
+
 /** A parser for Arm namespace objects.
 */
 STATIC CONST CM_OBJ_PARSER_ARRAY  ArmNamespaceObjectParser[] = {
@@ -778,7 +789,8 @@ STATIC CONST CM_OBJ_PARSER_ARRAY  ArmNamespaceObjectParser[] = {
   { "EArmObjMscNodeInfo",                  NULL,                                  0                                },
   { "EArmObjResNodeInfo",                  NULL,                                  0                                },
   { "EArmObjFuncDepInfo",                  NULL,                                  0                                },
-  { "EArmObjTpm2InterfaceInfo",            NULL,                                  0                                },
+  { "EArmObjTpm2InterfaceInfo",            CmArmTpm2InterfaceInfo,
+    ARRAY_SIZE (CmArmTpm2InterfaceInfo) },
   { "EArmObjMax",                          NULL,                                  0                                },
 };
 
