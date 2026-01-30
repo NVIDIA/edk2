6a28bdf90791ab1272460beb7564e69b9bb0dcbd
Ashish Singhal <ashishsingha@nvidia.com>
2026-01-30 13:50:11 -0700

    DynamicTablesPkg/SmbiosType38Lib: Support multiple IPMI devices
    Convert SmbiosType38Generator from single-instance to multi-instance
    pattern using TableEx functions. This allows generating multiple
    SMBIOS Type 38 entries when multiple CM_SMBIOS_IPMI_DEVICE_INFO
    objects are provided by the Configuration Manager.
    
    This change is required for multi-socket systems where each socket
    has its own SSIF interface to the BMC.
    
    Signed-off-by: Ashish Singhal <ashishsingha@nvidia.com>
    Tested-by: svc-sw-mobile-l4t <svc-sw-mobile-l4t@nvidia.com>

--- DynamicTablesPkg/Library/Smbios/SmbiosType38Lib/SmbiosType38Generator.c
+++ DynamicTablesPkg/Library/Smbios/SmbiosType38Lib/SmbiosType38Generator.c
@@ -1,7 +1,7 @@
 /** @file
   SMBIOS Type38 Table Generator.
 
-  Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+  Copyright (c) 2024 - 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
   Copyright (c) 2020 - 2021, Arm Limited. All rights reserved.<BR>
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
@@ -40,23 +40,42 @@ GET_OBJECT_LIST (
   @param [in]  SmbiosTableInfo Pointer to the SMBIOS table information.
   @param [in]  CfgMgrProtocol  Pointer to the Configuration Manager
                                Protocol interface.
-  @param [in]  Table           Pointer to the generated SMBIOS table.
+  @param [in]  Table           Pointer to the array of generated SMBIOS tables.
+  @param [in]  CmObjectToken   Pointer to the array of CM object tokens.
+  @param [in]  TableCount      Number of tables in the array.
 
   @return  EFI_SUCCESS If freed successfully or other failure codes
                         as returned by the generator.
 **/
 STATIC
 EFI_STATUS
-FreeSmbiosType38Table (
-  IN      CONST SMBIOS_TABLE_GENERATOR                   *CONST   Generator,
-  IN      CONST EDKII_DYNAMIC_TABLE_FACTORY_PROTOCOL     *CONST   TableFactoryProtocol,
-  IN      CONST CM_STD_OBJ_SMBIOS_TABLE_INFO             *CONST   SmbiosTableInfo,
-  IN      CONST EDKII_CONFIGURATION_MANAGER_PROTOCOL     *CONST   CfgMgrProtocol,
-  IN      SMBIOS_STRUCTURE                               **CONST  Table
+FreeSmbiosType38TableEx (
+  IN      CONST SMBIOS_TABLE_GENERATOR                *CONST    Generator,
+  IN      CONST EDKII_DYNAMIC_TABLE_FACTORY_PROTOCOL  *CONST    TableFactoryProtocol,
+  IN      CONST CM_STD_OBJ_SMBIOS_TABLE_INFO          *CONST    SmbiosTableInfo,
+  IN      CONST EDKII_CONFIGURATION_MANAGER_PROTOCOL  *CONST    CfgMgrProtocol,
+  IN      SMBIOS_STRUCTURE                            ***CONST  Table,
+  IN      CM_OBJECT_TOKEN                                       **CmObjectToken,
+  IN      CONST UINTN                                           TableCount
   )
 {
-  if (*Table != NULL) {
-    FreePool (*Table);
+  UINTN             Index;
+  SMBIOS_STRUCTURE  **TableList;
+
+  TableList = *Table;
+
+  if (TableList != NULL) {
+    for (Index = 0; Index < TableCount; Index++) {
+      if (TableList[Index] != NULL) {
+        FreePool (TableList[Index]);
+      }
+    }
+
+    FreePool (TableList);
+  }
+
+  if (*CmObjectToken != NULL) {
+    FreePool (*CmObjectToken);
   }
 
   return EFI_SUCCESS;
@@ -71,7 +90,9 @@ FreeSmbiosType38Table (
   @param [in]  SmbiosTableInfo Pointer to the SMBIOS table information.
   @param [in]  CfgMgrProtocol  Pointer to the Configuration Manager
                                Protocol interface.
-  @param [out] Table           Pointer to the SMBIOS table.
+  @param [out] Table           Pointer to the array of SMBIOS tables.
+  @param [out] CmObjectToken   Pointer to the array of CM object tokens.
+  @param [out] TableCount      Number of tables generated.
   @retval EFI_SUCCESS            Table generated successfully.
   @retval EFI_BAD_BUFFER_SIZE    The size returned by the Configuration
                                  Manager is less than the Object size for
@@ -83,76 +104,104 @@ FreeSmbiosType38Table (
 **/
 STATIC
 EFI_STATUS
-BuildSmbiosType38Table (
-  IN  CONST SMBIOS_TABLE_GENERATOR                         *Generator,
-  IN  CONST EDKII_DYNAMIC_TABLE_FACTORY_PROTOCOL   *CONST  TableFactoryProtocol,
-  IN  CM_STD_OBJ_SMBIOS_TABLE_INFO                 *CONST  SmbiosTableInfo,
-  IN  CONST EDKII_CONFIGURATION_MANAGER_PROTOCOL   *CONST  CfgMgrProtocol,
-  OUT SMBIOS_STRUCTURE                                     **Table,
-  OUT CM_OBJECT_TOKEN                                      *CmObjectToken
+BuildSmbiosType38TableEx (
+  IN  CONST SMBIOS_TABLE_GENERATOR                        *Generator,
+  IN  CONST EDKII_DYNAMIC_TABLE_FACTORY_PROTOCOL  *CONST  TableFactoryProtocol,
+  IN        CM_STD_OBJ_SMBIOS_TABLE_INFO          *CONST  SmbiosTableInfo,
+  IN  CONST EDKII_CONFIGURATION_MANAGER_PROTOCOL  *CONST  CfgMgrProtocol,
+  OUT       SMBIOS_STRUCTURE                              ***Table,
+  OUT       CM_OBJECT_TOKEN                               **CmObjectToken,
+  OUT       UINTN                                 *CONST  TableCount
   )
 {
   EFI_STATUS                  Status;
-  SMBIOS_TABLE_TYPE38         *SmbiosRecord;
+  UINT32                      NumIpmiDevices;
+  SMBIOS_STRUCTURE            **TableList;
+  CM_OBJECT_TOKEN             *CmObjectList;
   CM_SMBIOS_IPMI_DEVICE_INFO  *IpmiDeviceInfo;
+  UINTN                       Index;
+  SMBIOS_TABLE_TYPE38         *SmbiosRecord;
 
   ASSERT (Generator != NULL);
   ASSERT (SmbiosTableInfo != NULL);
   ASSERT (CfgMgrProtocol != NULL);
   ASSERT (Table != NULL);
+  ASSERT (TableCount != NULL);
   ASSERT (SmbiosTableInfo->TableGeneratorId == Generator->GeneratorID);
 
-  *Table = NULL;
+  *Table         = NULL;
+  *CmObjectToken = NULL;
+
   Status = GetESmbiosObjIpmiDeviceInfo (
              CfgMgrProtocol,
              CM_NULL_TOKEN,
              &IpmiDeviceInfo,
-             NULL
+             &NumIpmiDevices
              );
-
   if (EFI_ERROR (Status)) {
     DEBUG ((
       DEBUG_ERROR,
-      "%a: Failed to get Ipmi Device CM Object %r\n",
+      "%a: Failed to get IPMI Device CM Object %r\n",
       __FUNCTION__,
       Status
       ));
     return Status;
   }
 
-  //
-  // Two zeros following the last string.
-  //
-  SmbiosRecord = AllocateZeroPool (sizeof (SMBIOS_TABLE_TYPE38) + 2);
-  if (SmbiosRecord == NULL) {
-    DEBUG ((DEBUG_ERROR, "%a: memory allocation failed for smbios type38 record\n", __FUNCTION__));
-    Status = EFI_OUT_OF_RESOURCES;
-    goto exit;
+  TableList = (SMBIOS_STRUCTURE **)AllocateZeroPool (sizeof (SMBIOS_STRUCTURE *) * NumIpmiDevices);
+  if (TableList == NULL) {
+    DEBUG ((DEBUG_ERROR, "%a: Failed to allocate memory for IPMI device table list\n", __FUNCTION__));
+    return EFI_OUT_OF_RESOURCES;
   }
 
-  //
-  // Setup SMBIOS header
-  //
-  SmbiosRecord->Hdr.Type   = SMBIOS_TYPE_IPMI_DEVICE_INFORMATION;
-  SmbiosRecord->Hdr.Length = sizeof (SMBIOS_TABLE_TYPE38);
+  // Init pointers for FreeSmbiosType38TableEx
+  *Table      = TableList;
+  *TableCount = NumIpmiDevices;
+  for (Index = 0; Index < NumIpmiDevices; Index++) {
+    TableList[Index] = NULL;
+  }
 
-  //
-  // Fill in other fields of SMBIOS table
-  //
-  SmbiosRecord->InterfaceType                     = IpmiDeviceInfo->IpmiIntfType;
-  SmbiosRecord->IPMISpecificationRevision         = IpmiDeviceInfo->IpmiSpecRevision;
-  SmbiosRecord->I2CSlaveAddress                   = IpmiDeviceInfo->IpmiI2CSlaveAddress;
-  SmbiosRecord->NVStorageDeviceAddress            = IpmiDeviceInfo->IpmiNVStorageDevAddress;
-  SmbiosRecord->BaseAddress                       = IpmiDeviceInfo->IpmiBaseAddress;
-  SmbiosRecord->BaseAddressModifier_InterruptInfo = IpmiDeviceInfo->IpmiBaseAddModIntInfo;
-  SmbiosRecord->InterruptNumber                   = IpmiDeviceInfo->IpmiInterruptNum;
+  CmObjectList = (CM_OBJECT_TOKEN *)AllocateZeroPool (sizeof (CM_OBJECT_TOKEN) * NumIpmiDevices);
+  if (CmObjectList == NULL) {
+    DEBUG ((DEBUG_ERROR, "%a: Failed to allocate memory for CmObjectList\n", __FUNCTION__));
+    return EFI_OUT_OF_RESOURCES;
+  }
 
-  *Table         = (SMBIOS_STRUCTURE *)SmbiosRecord;
-  *CmObjectToken = IpmiDeviceInfo->IpmiDeviceInfoToken;
-  Status         = EFI_SUCCESS;
+  // Init pointers for FreeSmbiosType38TableEx
+  *CmObjectToken = CmObjectList;
 
-exit:
-  return Status;
+  for (Index = 0; Index < NumIpmiDevices; Index++) {
+    //
+    // Two zeros following the last string.
+    //
+    SmbiosRecord = AllocateZeroPool (sizeof (SMBIOS_TABLE_TYPE38) + 2);
+    if (SmbiosRecord == NULL) {
+      DEBUG ((DEBUG_ERROR, "%a: memory allocation failed for smbios type38 record\n", __FUNCTION__));
+      return EFI_OUT_OF_RESOURCES;
+    }
+
+    //
+    // Setup SMBIOS header
+    //
+    SmbiosRecord->Hdr.Type   = SMBIOS_TYPE_IPMI_DEVICE_INFORMATION;
+    SmbiosRecord->Hdr.Length = sizeof (SMBIOS_TABLE_TYPE38);
+
+    //
+    // Fill in other fields of SMBIOS table
+    //
+    SmbiosRecord->InterfaceType                     = IpmiDeviceInfo[Index].IpmiIntfType;
+    SmbiosRecord->IPMISpecificationRevision         = IpmiDeviceInfo[Index].IpmiSpecRevision;
+    SmbiosRecord->I2CSlaveAddress                   = IpmiDeviceInfo[Index].IpmiI2CSlaveAddress;
+    SmbiosRecord->NVStorageDeviceAddress            = IpmiDeviceInfo[Index].IpmiNVStorageDevAddress;
+    SmbiosRecord->BaseAddress                       = IpmiDeviceInfo[Index].IpmiBaseAddress;
+    SmbiosRecord->BaseAddressModifier_InterruptInfo = IpmiDeviceInfo[Index].IpmiBaseAddModIntInfo;
+    SmbiosRecord->InterruptNumber                   = IpmiDeviceInfo[Index].IpmiInterruptNum;
+
+    TableList[Index]    = (SMBIOS_STRUCTURE *)SmbiosRecord;
+    CmObjectList[Index] = IpmiDeviceInfo[Index].IpmiDeviceInfoToken;
+  }
+
+  return EFI_SUCCESS;
 }
 
 /** The interface for the SMBIOS Type38 Table Generator.
@@ -166,12 +215,14 @@ SMBIOS_TABLE_GENERATOR  SmbiosType38Generator = {
   L"SMBIOS.TYPE38.GENERATOR",
   // SMBIOS Table Type
   EFI_SMBIOS_TYPE_IPMI_DEVICE_INFORMATION,
-  // Build table function.
-  BuildSmbiosType38Table,
-  // Free function.
-  FreeSmbiosType38Table,
+  // Build table function (unused - using Extended).
   NULL,
-  NULL
+  // Free function (unused - using Extended).
+  NULL,
+  // Build table function Extended.
+  BuildSmbiosType38TableEx,
+  // Free function Extended.
+  FreeSmbiosType38TableEx
 };
 
 /** Register the Generator with the SMBIOS Table Factory.
